ddf300a39013bab9bc8f27c292290e3aa3bc0087
Bryant <bryant.brock@anedot.com>
2022-04-08 10:29:11 -0500

    add heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const remix = require('@remix-run/express')
 const app = express()
+const port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/public'));
 app.all('*', remix.createRequestHandler({ build: require('./build') }))
 
 // DEV MODE
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}!`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
 });
